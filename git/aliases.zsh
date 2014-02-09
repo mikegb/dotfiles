5bc70e806dc9670c13c9bf3f9495610017a1acef
Mike Brown <m.brown@techsmith.com>
2014-02-08 21:16:40 -0500

    Update git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,8 +7,9 @@ then
 fi
 
 # The rest of my fun git aliases
-alias gl='git pull --prune'
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gpull='git pull --prune'
+alias glog="git log --decorate --graph --oneline --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gla='git log --all --oneline --decorate --graph'
 alias gp='git push origin HEAD'
 alias gd='git diff'
 alias gc='git commit'
@@ -16,6 +17,13 @@ alias gca='git commit -a'
 alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
+alias gitref='git reflog --decorate'
+alias glc='git log -p -n1'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{\$1=\$2=\"\"; print \$0}' | \
            perl -pe 's/^[ \t]*//' | sed 's/ /\\\\ /g' | xargs git rm"
+
+
+# Hub aliases
+alias hb="hub browse"
+alias hi="hub browse -- issues"
